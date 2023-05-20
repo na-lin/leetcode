b371984011c9ab758a3b3da26748536db212da6d
Na Lin <lna483018@gmail.com>
2023-05-19 23:19:06 -0400

    add test case

--- algorithm/Divide_Conquer.js
+++ algorithm/Divide_Conquer.js
@@ -10,4 +10,7 @@ common divide and conquer algorithm: Merge sort, quick sort, and binary search
 /**
 Given an array nums of size n, return the majority element.
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Input: nums = [3,2,3]
+Output: 3
  */
