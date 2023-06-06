669c9c8c5b0de11bdf4a53302323b9396ee6a6af
Na Lin <lna483018@gmail.com>
2023-06-05 22:28:43 -0400

    leetcode 3sum

---
+++ hot100/Leetcode15.js
@@ -0,0 +1,33 @@
+/**
+15. 3Sum
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ */
+
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  const res = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) return res;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let l = i + 1;
+    let r = nums.length - 1;
+    while (l < r) {
+      const target = 0 - nums[i];
+      if (nums[l] + nums[r] === target) {
+        res.push([nums[i], nums[l], nums[r]]);
+        while (l < r && nums[l] === nums[l + 1]) l++;
+        while (l < r && nums[r] === nums[r - 1]) r--;
+        l++;
+        r--;
+      } else if (nums[l] + nums[r] < target) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+  }
+  return res;
+}
